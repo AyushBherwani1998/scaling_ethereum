05d054e984074f86365d3ff0789c035400693947
AyushBherwani1998 <ayush.bherwani1998@gmail.com>
2024-04-10 14:46:29 +0530

    initial commit

---
+++ frontend/frontend/frontendApp.swift
@@ -0,0 +1,17 @@
+//
+//  frontendApp.swift
+//  frontend
+//
+//  Created by Ayush B on 10/04/24.
+//
+
+import SwiftUI
+
+@main
+struct frontendApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
