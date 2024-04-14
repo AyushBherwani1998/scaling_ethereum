de6990d4b7d6638b35d520619e04f88c08c5c681
AyushBherwani1998 <ayush.bherwani1998@gmail.com>
2024-04-14 16:07:24 +0530

    add mpc recovery contract using sign protocol

--- attestation/contracts/MpcRecovery.sol
+++ attestation/contracts/MpcRecovery.sol
@@ -3,17 +3,18 @@ pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
-import { ISP } from "@ethsign/sign-protocol-evm/src/interfaces/ISP.sol";
-import { Attestation } from "@ethsign/sign-protocol-evm/src/models/Attestation.sol";
-import { DataLocation } from "@ethsign/sign-protocol-evm/src/models/DataLocation.sol";
+import "@openzeppelin/contracts/utils/cryptography/MessageHashUtils.sol";
+import {ISP} from "@ethsign/sign-protocol-evm/src/interfaces/ISP.sol";
+import {Attestation} from "@ethsign/sign-protocol-evm/src/models/Attestation.sol";
+import {DataLocation} from "@ethsign/sign-protocol-evm/src/models/DataLocation.sol";
 
 contract MpcRecovery is Ownable {
     ISP public spInstance;
     uint64 public schemaId;
 
-    mapping(address recipient => uint64 attestationId) private mpcAccountAttestations;
+    mapping(address => uint64) private mpcAccountAttestations;
 
-    constructor() Ownable(_msgSender()) { }
+    constructor() Ownable(_msgSender()) {}
 
     function setSPInstance(address instance) external onlyOwner {
         spInstance = ISP(instance);
@@ -23,35 +24,38 @@ contract MpcRecovery is Ownable {
         schemaId = schemaId_;
     }
 
-    function claimMPCAccount(bytes32 _hash, bytes memory _signature) public view returns (bool) {
-        address signer = ECDSA.recover(_hash, _signature);
-        if(signer != msg.sender) {
+    function claimMPCAccount(
+        bytes32 _message,
+        bytes memory _signature,
+        address _owner
+    ) public view returns (bool) {
+        bytes32 hash = MessageHashUtils.toEthSignedMessageHash(_message);
+        address signer = ECDSA.recover(hash, _signature);
+        if (signer != _owner) {
             revert("Signature does not match message sender");
-        } 
+        }
 
-        return mpcAccountAttestations[signer] != 0;
+        return mpcAccountAttestations[_owner] != 0;
     }
 
-
     function attestMPCAccount(address recipient, bytes memory data) external {
-    
-         bytes[] memory recipients = new bytes[](1);
-            recipients[0] = abi.encode(recipient);
+        bytes[] memory recipients = new bytes[](1);
+        recipients[0] = abi.encode(recipient);
 
-            Attestation memory a = Attestation({
-                schemaId: schemaId,
-                linkedAttestationId: 0,
-                attestTimestamp: 0,
-                revokeTimestamp: 0,
-                attester: address(this),
-                validUntil: 0,
-                dataLocation: DataLocation.ONCHAIN,
-                revoked: false,
-                recipients: recipients,
-                data: data 
-            });
+        Attestation memory a = Attestation({
+            schemaId: schemaId,
+            linkedAttestationId: 0,
+            attestTimestamp: 0,
+            revokeTimestamp: 0,
+            attester: address(this),
+            validUntil: 0,
+            dataLocation: DataLocation.ONCHAIN,
+            revoked: false,
+            recipients: recipients,
+            data: data
+        });
 
-            uint64 attestationId = spInstance.attest(a, "", "", "");
-            mpcAccountAttestations[recipient] = attestationId;
+        uint64 attestationId = spInstance.attest(a, "", "", "");
+        mpcAccountAttestations[recipient] = attestationId;
     }
 }
