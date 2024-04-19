f0f8d9e989e7495e2b6cf058604edd5bc02df951
AyushBherwani1998 <ayush.bherwani1998@gmail.com>
2024-04-19 16:32:21 +0530

    add recovery option

---
+++ frontend/frontend/Views/RecoveryView.swift
@@ -0,0 +1,18 @@
+//
+//  RecoveryView.swift
+//  frontend
+//
+//  Created by Ayush B on 19/04/24.
+//
+
+import SwiftUI
+
+struct RecoveryView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RecoveryView()
+}
